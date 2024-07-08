6b1e5300f4b5c5db1124deedee8c2ad23bb491a1
Dora-perry <theodorachinonso@gmail.com>
2024-07-08 16:44:30 +0100

    Design Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="w-64 h-screen bg-gray-800 text-white">
+      <div className="p-4">My Dashboard</div>
+      <ul>
+        <li>
+          <Link to="/" className="block p-4 hover:bg-gray-700">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/add-contact" className="block p-4 hover:bg-gray-700">Add Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
