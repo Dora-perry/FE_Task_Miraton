bab20ef07c5b1a69dfcd2e2a6e01893d6d3be524
Dora-perry <theodorachinonso@gmail.com>
2024-07-08 16:31:51 +0100

    Folder structure and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import "./App.css";
-import "./tailwind.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard';
+import AddContact from './pages/AddContact';
 
 function App() {
   return (
-    <div className="App">
-      <p className="text-red-500 font-bold">Theodora</p>
-    </div>
+    <Router>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-grow p-6">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/add-contact" element={<AddContact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
